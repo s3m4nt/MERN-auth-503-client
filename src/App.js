30cb1a7a1bb86640263b5d1345f993a659d9b4db
Brian Young <berkshirehunt@gmail.com>
2021-07-08 10:19:55 -0700

    React app stubbed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
+import Login from ('./components/Login')
+import Navbar from ('./components/Navbar')
+import Profile from ('./components/Profile')
+import Register from ('./components/Register')
+import Welcome from ('./components/Welcome')
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Router>
+      <header>
+        <Navbar />
       </header>
-    </div>
+      <div className="App">
+        <Switch>
+        
+        <Route 
+          exact path="/"
+          component={Welcome}
+        />
+
+
+        <Route
+        path="/register"
+        render={ (props) => <Register {...props}  /> }
+/>
+
+        <Route
+        path="/login"
+        render={ (props) => <Login {...props}  /> }
+/>
+        <Route
+        path="/profile"
+        render={ (props) => <Profile {...props}  /> }
+/>
+
+
+
+
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
