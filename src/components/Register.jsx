70993d6d0537b911b82fe322425d204c2670e439
Brian Young <berkshirehunt@gmail.com>
2021-07-08 15:07:33 -0700

    reg 2

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,66 +1,99 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import axios from 'axios'
 import jwt from 'jsonwebtoken'
 import { Redirect } from 'react-router-dom'
-import Profile from './Profile'
+import Profile from './Profile.jsx'
 
 export default function Register(props) {
-    // state for the controlled form
-    const [name, setName] = useState('')
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
+  // state for the controlled from
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  // state for the flash message from the server
+  const [message, setMessage] = useState('')
 
-    // state for the flash message from the server
-    const [message, setMessage] = useState('')
+  // function to handle form submission
+  const handleSubmit = async e => {
+      try{
 
-    // function to handle form submission
-    const handleSubmit = e => {
-        e.preventDefault()
-        console.log('submit the form 🏴󠁧󠁢󠁥󠁮󠁧󠁿');
-    }
-    // redirect if the user is logged in
+          e.preventDefault()
+          // make a request body
+            const requestBody = {
+                name: name,
+                email: email,
+                password: password
+            }
 
-    return(
-        <div>
-            <h3>Registration Form</h3>
-            <form onSubmit={handleSubmit}>
+          // post reg data to server
+            const response = await axios.post(`${process.env.REACT_APP_SERVER_URL}/api-v1/users/register`, requestBody)
 
-<label htmlFor='name-input'>Name: </label>
-<input                
-id='name-input'
-type='text'
-placeholder='enter your name'
-onChange={e => setName(e.target.value)}
-value={name}
-/>
+          // take token out of the response
+            const { token } = response.data
 
-<label htmlFor='email-input'>Email: </label>
-<input                
-id='email-input'
-type='email'
-placeholder='enter your email'
-onChange={e => setEmail(e.target.value)}
-value={email}
-/>
+        //  set token in local storage
+            localStorage.setItem('jwtToken', token)
 
+          // decode the token
+            const decoded = jwt.decode(token)
 
-<label htmlFor='password-input'>password: </label>
-<input                
-id='password-input'
-type='password'
-placeholder='desired password'
-onChange={e => setPassword(e.target.value)}
-value={password}
-/>
+          // set the user in the App.js state
+            props.setCurrentUser(decoded)
 
+      }
+      catch(err){
+          // set message if the error is 400
+          if(err.response.status === 400){
+              setMessage(err.response.data.message)
+          } else {
+              console.log(err);
+          }
+      }
+      console.log('submit the form! 🌽')
+  }
 
+  // redirect if the user is logged in
+  return (
+    <div>
+      <h3>Registration form:</h3>
 
-                <input 
+        <p>{message}</p>
 
-                type='submit'
-                value='Make new account'
-                />
-            </form>
-        </div>
-    )
+      <form onSubmit={handleSubmit}>
+        <label htmlFor='name-input'>Name:</label>
+
+        <input 
+          id='name-input'
+          type='text'
+          placeholder='enter your name user...'
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+
+        <label htmlFor='email-input'>email:</label>
+
+        <input 
+          id='email-input'
+          type='email'
+          placeholder='enter your email user...'
+          onChange={e => setEmail(e.target.value)}
+          value={email}
+        />
+
+        <label htmlFor='password-input'>password:</label>
+
+        <input 
+          id='password-input'
+          type='password'
+          placeholder='desired password'
+          onChange={e => setPassword(e.target.value)}
+          value={password}
+        />
+
+        <input 
+          type='submit'
+          value='make new account'
+        />
+      </form>
+    </div>
+  )
 }
